a6a9dfc74715cd331381f96c4ffe122bfeb7b244
jwrigl <124375480+jwrigl@users.noreply.github.com>
2023-07-07 13:44:35 +0100

    Change empty box character in board list, amend buildboard function

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
 const gameBoard = () => {
     let board = [
-        '-', '-', '-',
-        '-', '-', '-',
-        '-', '-', '-'
+        '', '', '',
+        '', '', '',
+        '', '', ''
       ];
-    
-    
-}
+    function newBoard() {
+        board = [
+            '', '', '',
+            '', '', '',
+            '', '', ''
+          ];
+    }
+    function buildBoard(board) {
+        parentContainer = document.querySelector('#boardContainer');
+        for (let i = 0; i < board.length; i++) {
+            let cell = document.createElement('div');
+            cell.classList.add('cell');
+            parentContainer.appendChild(cell);
+            cell.innerText = board[i];
+        }
+
+    }
+    function destroyBoard(board) {
+
+    }
+};
