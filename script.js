b9578cc6dc6f5097103b82a78a81f0aa6eea2499
jwrigl <124375480+jwrigl@users.noreply.github.com>
2023-07-09 16:39:41 +0100

    edit gameBoard function

--- script.js
+++ script.js
@@ -2,15 +2,17 @@ const player = () => {
     //player factory function
 }
 
-const gameBoard = () => {
-    //could be a factory function but not required
+function gameBoard() {
+    //gameboard factory function
+    return {
+        boardData: ["", "", "", "", "", "", "", "", ""],
+        resetBoardData: function () {
+            this.boardData = ["", "", "", "", "", "", "", "", ""];
+        }
 
-    //this will be part of an object it will be stored as a memory address
-    board: ["", "", "", "", "", "", "", "", ""];
-    
-    
-    
+    }
 }
+
 
 const gameLogic = () => {
     //Not a factory function
